f73f01342f5a78034de3bcb0cebfa684d86ee6c3
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-02-27 13:11:17 +0000

    se resolvio hasta el ejercicio 7.1

--- exercises/04.2-Loop_from_seven_to_twelve/app.js
+++ exercises/04.2-Loop_from_seven_to_twelve/app.js
@@ -1,1 +1,4 @@
 // Your code here
+for(let num = 7; num <=12; num++){
+    console.log(num);
+}
--- exercises/04.3-Add_items_to_array/app.js
+++ exercises/04.3-Add_items_to_array/app.js
@@ -1,5 +1,10 @@
 let arr = [4,5,734,43,45];
 
 // Your code here, use the push() function and the 2 Math functions
+let num1 = Math.floor(Math.random() * 100);
+let num2 = Math.floor(Math.random() * 100);
+arr.push(num2); 
+arr.push(num1);
+
 
 console.log(arr);
